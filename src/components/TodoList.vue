09ac82ee259054472a84708d0e5e7e2936721643
OG'ABEK <shomoyxhacker007@mail.ru>
2023-03-25 20:47:06 +0500

    feat: open and done status added

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,44 +1,86 @@
 <script setup>
 import Logo from '@/components/icons/Logo.vue'
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 const todo = ref('')
-const todos = ref(['Drink Coffee', 'Learn JS'])
+const todos = ref([
+  {
+    task: 'Drink Coffee',
+    isOpen: true
+  },
+  {
+    task: 'Learn JS',
+    isOpen: false
+  }
+])
 
 const addTodo = () => {
-  todos.value.push(todo.value)
+  if (todo.value) {
+    todos.value.push({
+      task: todo.value,
+      isOpen: true
+    })
+    todo.value = ''
+  }
 }
 
 const removeTodo = (task) => {
   todos.value = todos.value.filter(td => !td.includes(task))
 }
 
+
+const changeTaskStatus = (task) => {
+  todos.value = todos.value.map(td => {
+    if (td.task.includes(task)) {
+      return {
+        task: td.task,
+        isOpen: !td.isOpen
+      }
+    } else {
+      return td
+    }
+  })
+}
+
+const allTasks = computed(() => {
+  return todos.value.filter(td => td.isOpen).length
+})
+
+const completedTasks = computed(() => {
+  return todos.value.filter(td => !td.isOpen).length
+})
+
 </script>
 <template>
-  <div class="bg-blue-300 h-screen flex justify-center items-center flex-col space-y-5">
+  <div class="flex flex-col items-center justify-center h-screen space-y-5 bg-blue-300">
     <h1 class="flex items-center font-extrabold text-white">
       <Logo class="mr-1" /> TODO LIST
     </h1>
     <div class="bg-white w-[250px] rounded-xl p-3 shadow-xl">
       <div class="flex items-center space-x-2">
         <input type="text" v-model="todo"
-          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+          class="block w-full p-1 text-sm text-gray-900 border border-gray-300 rounded-lg shadow-sm bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
           placeholder="Enter task...">
-        <button @click="addTodo" class="bg-blue-600 flex text-white items-center rounded-lg shadow p-0.5 px-3 whitespace-nowrap">
+        <button @click="addTodo"
+          class="bg-blue-600 flex text-white items-center rounded-lg shadow p-0.5 px-3 whitespace-nowrap">
           + Add
         </button>
       </div>
     </div>
     <div class="bg-white w-[250px] rounded-xl p-3 shadow-xl">
-      <div class="bg-gray-100 rounded-full px-2 flex items-center py-1 justify-between mb-3">
-        <span class="bg-green-200 rounded-full text-green-500 px-1 text-[10px]">1 tasks</span>
-        <span class="bg-orange-200 rounded-full text-orange-500 px-1 text-[10px]">1 done</span>
+      <div class="flex items-center justify-between px-2 py-1 mb-3 bg-gray-100 rounded-full">
+        <span class="bg-green-200 rounded-full text-green-500 px-1 text-[10px]">{{ allTasks }} open</span>
+        <span class="bg-orange-200 rounded-full text-orange-500 px-1 text-[10px]">{{ completedTasks }} done</span>
       </div>
-      <ul class="overflow-y-auto overflow-x-hidden h-[25vh]">
-        <li v-for="(task, idx) in todos" class="hover:bg-gray-100 cursor-pointer rounded-lg px-4 py-2">
-          {{ idx + 1 }}. {{ task }}
-        </li>
-      </ul>
+      <div class="overflow-y-auto overflow-x-hidden h-[25vh]">
+        <div v-for="(todo, idx) in todos" :key="idx">
+          <div :for="`task${idx}`" @click="changeTaskStatus(todo.task)"
+            class="block px-4 py-2 rounded-lg cursor-pointer hover:bg-gray-100" :class="{ 'line-through': !todo.isOpen }">
+            <input type="checkbox" :checked="!todo.isOpen">
+            {{ todo.task }}
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
